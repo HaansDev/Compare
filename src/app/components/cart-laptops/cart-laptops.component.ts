2ab7f156bbb2bc8a4a7d6f0684d3cf24645b6f17
Cristian Fdez <haansdev@gmail.com>
2021-05-24 08:56:44 +0200

    input range price, button scroll to top and button add cart compare in progress

---
+++ src/app/components/cart-laptops/cart-laptops.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-laptops',
+  templateUrl: './cart-laptops.component.html',
+  styleUrls: ['./cart-laptops.component.scss']
+})
+export class CartLaptopsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
