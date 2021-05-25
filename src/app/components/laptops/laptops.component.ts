d37d2e499ba13adbeee9759f63012b72fbf3a66a
josepebel <josepe.beldad@gmail.com>
2021-05-25 21:34:27 +0200

    query filter

--- src/app/components/laptops/laptops.component.ts
+++ src/app/components/laptops/laptops.component.ts
@@ -18,21 +18,57 @@ export class LaptopsComponent implements OnInit {
     totalPages!: number;
     pages: Array<number> = [];
 
+
+   filter!: string;
+   minPrice!: string;
+   maxPrice!: string;
+   cpuAMD!: string;
+   cpuINTEL!: string;
+   cpuMONE!: string;
+   graphicsAMD!: string;
+   graphicsNVIDIA!: string;
+   graphicsINTEGRATED!: string;
+   romHDD!: string;
+   romSSD!: string;
+   ram_typeTHREE!: string;
+   ram_typeFOUR!: string;
+
+
     constructor(private activateRoute: ActivatedRoute, private router: Router, private laptopsService: LaptopsService) { }
 
     ngOnInit(): void {
-        this.activateRoute.params.subscribe( params => {
-            this.page = params.page
-          })
-          this.getPullLaptops()
-          this.getPages()
+      this.filter = ""
+      this.minPrice = ""
+      this. maxPrice = ""
+      this.cpuAMD = ""
+      this.cpuINTEL = ""
+      this.cpuMONE = ""
+      this.graphicsAMD = ""
+      this.graphicsNVIDIA = ""
+      this.graphicsINTEGRATED = ""
+      this.romHDD = ""
+      this.romSSD = ""
+      this.ram_typeTHREE = ""
+      this.ram_typeFOUR = ""
+
+
+      this.activateRoute.params.subscribe( params => {
+        this.page = params.page
+      })
+      this.getPullLaptops()
+      this.getPages()
     }
+
+
     onSliderChange(selectedValues: number[]) {
         this.currentValues = selectedValues;
+        console.log(this.currentValues)
     }
+
     detailLaptop(id: any) {
       this.router.navigate(["laptop/" + id ])
     }
+
     getPullLaptops()  {
         this.laptopsService.getPaginationLaptops(this.page).subscribe(
           (data: Laptop[]) => {
@@ -44,8 +80,10 @@ export class LaptopsComponent implements OnInit {
           }
         );
     }
+
+
     getPages() {
-        this.laptopsService.getAllLaptops().subscribe(
+      this.laptopsService.getLaptops(this.filter, this.minPrice, this.maxPrice, this.cpuAMD, this.cpuINTEL, this.cpuMONE, this.graphicsAMD,this.graphicsNVIDIA, this.graphicsINTEGRATED, this.romHDD, this.romSSD, this.ram_typeTHREE, this.ram_typeFOUR).subscribe(
             (data: Laptop[]) => {
                 this.allLaptops = data;
                 this.totalPages = Math.ceil(this.allLaptops.length/12);
@@ -56,6 +94,8 @@ export class LaptopsComponent implements OnInit {
             }
         );
     }
+
+
     goPage(page: number) {
         this.router.navigate(['/laptops/' + page]);
         this.activateRoute.params.subscribe(params => {
@@ -81,5 +121,32 @@ export class LaptopsComponent implements OnInit {
         } else{
             this.goPage(1)
         }
+    }
+
+
+
+    laptops: Array<Laptop> = [];
+
+
+    loadLaptops(){
+      let filter = ""
+      let minPrice = ""
+      let maxPrice = ""
+      let graphics = ""
+      let rom = ""
+      let cpu = ""
+      let ram_type = ""
+
+
+
+      this.laptopsService.getLaptops(this.filter, this.minPrice, this.maxPrice, this.cpuAMD, this.cpuINTEL, this.cpuMONE, this.graphicsAMD,this.graphicsNVIDIA, this.graphicsINTEGRATED, this.romHDD, this.romSSD, this.ram_typeTHREE, this.ram_typeFOUR).subscribe(
+        (data: Laptop[]) => {
+          this.allLaptops = data;
+          console.log(data);
+        },
+        (error) => {
+          console.log('Error:', error);
+        }
+      );
     }
 }
